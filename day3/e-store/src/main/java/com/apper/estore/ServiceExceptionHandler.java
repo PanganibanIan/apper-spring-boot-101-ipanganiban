ff52e319a8f5da35a3e19425febe5758fcfa1259
PanganibanIan <ian-p.panganiban@gcash.com>
2023-07-04 14:11:49 +0800

    Day 3 scratch after Lab - Docker setup for estoredb

--- day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
+++ day3/e-store/src/main/java/com/apper/estore/ServiceExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.apper.estore;
 
+import com.apper.estore.payload.ServiceError;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.security.Provider;
 
 @ControllerAdvice
 public class ServiceExceptionHandler {
